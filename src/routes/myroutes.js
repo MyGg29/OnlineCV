788a3de94766388e54b5f613f610d498aed1af0a
MyGg29 <gwendalgourmelon@gmail.com>
2020-01-19 18:25:57 +0100

    Social buttons & stuff

--- src/routes/myroutes.js
+++ src/routes/myroutes.js
@@ -3,14 +3,14 @@ var router = express.Router()
 
 // define the home page route
 router.get('/', function (req, res) {
-  res.render('CV')
+  res.render('CV2')
 })
 
 router.get('/OK', function (req, res) {
   res.send('OK')
 })
 router.get('/test', function(req,res){
-  res.render('CV2')
+  res.render('CV')
 })
 
 module.exports = router
